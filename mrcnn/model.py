3fcc580cc9f19378842edbe88c9c89a971e76ffc
greenlans <61324866+greenlans@users.noreply.github.com>
2020-05-17 01:59:17 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2187,7 +2187,7 @@ class MaskRCNN():
         # Compile
         self.keras_model.compile(
             optimizer=optimizer,
-            loss=[None] * len(self.keras_model.outputs))
+            loss=[None] * len(self.keras_model.outputs),metrics=['accuracy'])
 
         # Add metrics for losses
         for name in loss_names:
